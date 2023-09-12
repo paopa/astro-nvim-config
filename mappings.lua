eb8d830f3b753a27759bac3caaaa99fffcf49830
Pao-Sheng Wang <david840422@gmail.com>
2023-09-13 01:56:36 +0900

    customize key mapping for projects management plugin

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,12 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- Plugins Mappings
+    --
+    -- Plugin: ahmedkhalf/project.nvim
+    -- github: https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/project/project-nvim
+    ["<leader>fp"] = {"<cmd>Telescope projects<cr>", desc = "Find Projects"}
   },
   t = {
     -- setting a mapping to false will disable it
